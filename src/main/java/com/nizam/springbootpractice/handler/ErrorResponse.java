a4dfbaa8b931cdcb06abbc768ec2d53909b438cb
Nizam Abdul <abdulnijamudeen@gmail.com>
2021-05-12 11:34:42 +0530

    Included Error Response

---
+++ src/main/java/com/nizam/springbootpractice/handler/ErrorResponse.java
@@ -0,0 +1,16 @@
+package com.nizam.springbootpractice.handler;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorResponse {
+    private String message;
+    private String error;
+    private LocalDateTime dateTime;
+}
--- src/main/java/com/nizam/springbootpractice/handler/RestExceptionHandler.java
+++ src/main/java/com/nizam/springbootpractice/handler/RestExceptionHandler.java
@@ -1,19 +1,27 @@
 package com.nizam.springbootpractice.handler;
 
+import java.time.LocalDateTime;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import lombok.extern.log4j.Log4j2;
+
 @RestControllerAdvice
+@Log4j2
 public class RestExceptionHandler {
-    
+
     @ExceptionHandler
-    public ResponseEntity<String> handleRestException(RestException exception){
-        return new ResponseEntity<>(exception.getMessage(), exception.getStatus());
+    public ResponseEntity<ErrorResponse> handleRestException(RestException exception) {
+        log.error(exception);
+        ErrorResponse errorResponse = new ErrorResponse(exception.getMessage(), exception.getStatus().getReasonPhrase(), LocalDateTime.now());
+        return new ResponseEntity<>(errorResponse, exception.getStatus());
     }
-    
+
     @ExceptionHandler
-    public String handleException(Exception exception){
+    public String handleException(Exception exception) {
+        log.error(exception);
         return exception.getMessage();
     }
 }
